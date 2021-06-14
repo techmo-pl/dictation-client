a83e623d23e4d08450f1fd3c411bf7a1a56a6a3c
Jan Wozniak <jan.wozniak@techmo.pl>
2021-06-14 10:32:07 +0200

    Updating grpc build to provide smaller image

--- cpp/make_proto.sh
+++ cpp/make_proto.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # coding=utf-8
 
+set -eo pipefail
+
 grpc_root="/opt/grpc_v1.24.3"
 
-PROTOC="$grpc_root/third_party/protobuf/src/protoc"
-PLUGIN="--plugin=protoc-gen-grpc=$grpc_root/bins/opt/grpc_cpp_plugin"
+PROTOC="$grpc_root/build/third_party/protobuf/protoc"
+PLUGIN="--plugin=protoc-gen-grpc=$grpc_root/build/grpc_cpp_plugin"
+GAPIS="../submodules/googleapis"
 
 if [ ! -x ${PROTOC} ]; then
     echo "${PROTOC}; no such file."
     exit 1
 fi
 
-echo "Generating dictation C++ protobuf/grpc sources..."
+echo "Generating dictation C++ protobuf/grpc sources."
 path_i="../proto"
 path_o="libdictation-client"
 ${PROTOC}   -I${path_i} \
-            -I../submodules/googleapis \
+            -I${GAPIS} \
+            -I${GAPIS}/third_party/protobuf/src \
             ${PLUGIN} \
             --cpp_out=${path_o} \
             --grpc_out=${path_o} \
--- tools/install_grpc.sh
+++ tools/install_grpc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # coding=utf-8
 
-jobs=32
+jobs=4
 version="v1.24.3"
-[ $# -ge 1 ] && jobs=$1
+[ $# -ge 1 ] && jobs="$1"
 [ $# -ge 2 ] && version="$2"
 
 grpc_path="/opt/grpc_${version}"
@@ -21,24 +21,37 @@ fi
 # download and build
 cwd=`pwd`
 
-git clone https://github.com/grpc/grpc grpc_tmp
+git clone https://github.com/grpc/grpc grpc
+cd grpc
+git checkout "tags/v1.24.3"
+git submodule update --init
+cd third_party/protobuf/
+git checkout v3.11.3
+cd ../../..
 
-mv grpc_tmp "${grpc_path}"
+mv grpc "${grpc_path}"
+
 cd "${grpc_path}"
 
-git checkout "tags/${version}"
-git submodule update --init
+# cares
+cd third_party/cares/cares
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCARES_STATIC=ON ../
+make -j $jobs
+
+cd "${grpc_path}"
 
 # zlib
 cd third_party/zlib
-CFLAGS="-fPIC" ./configure
+CFLAGS="-fPIC" ./configure 
 make -j $jobs
 cd ../..
 
 # build grpc
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DgRPC_SSL_PROVIDER=package ..
 make -j $jobs
 
-#ln -sf ${grpc_path} /opt/grpc
-
 cd "${cwd}"
-
