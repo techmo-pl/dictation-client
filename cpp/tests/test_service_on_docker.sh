9502f88af3aa60078cfb6c215de051dfd3835483
Jan Wozniak <jan.wozniak@techmo.pl>
2021-07-21 19:48:09 +0200

    Adding tests for run-on-docker script

---
+++ cpp/tests/test_service_on_docker.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# coding=utf-8
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
+
+test_result="fail"
+test_service_address="demo.devtechmo.pl:51190"
+
+cp "${SCRIPTPATH}/data/test.wav" "${SCRIPTPATH}/../docker/wav/ci_tmp_test.wav"
+cp  "${SCRIPTPATH}/data/test_context.wav" "${SCRIPTPATH}/../docker/wav/ci_tmp_test_context.wav"
+
+test_wave="ci_tmp_test.wav"
+test_context_wave="ci_tmp_test_context.wav"
+
+function on_exit {
+
+    if [[ $test_result == "fail" ]]; then
+        echo "---------------------"
+        echo " SERVICE TEST FAILED!" 
+        echo "---------------------"
+    else
+        echo "-------------------------------------"
+        echo " SERVICE TEST COMPLETED SUCCESSFULLY!" 
+        echo "-------------------------------------"
+    fi
+    rm "${SCRIPTPATH}/../docker/wav/ci_tmp_test.wav" "${SCRIPTPATH}/../docker/wav/ci_tmp_test_context.wav"
+}
+trap on_exit EXIT
+
+
+function check_output () {
+
+    cmd="$1"
+    phrase="$2"
+
+    set +e
+    output=$({ eval "$cmd"  1>&2; }  2>&1 ) # we want grep over output whether the command succeeds or not
+    set -e
+    echo "${output}" | grep "$phrase" > /dev/null 2>&1; # if this line returns non-0-code, 'set -e' will cause exit 
+    echo "--> OK"
+}
+
+echo
+echo "Testing: basic recognition"
+cmd="${SCRIPTPATH}/../docker/run_dictation_client_cpp.sh --service-address=${test_service_address} --filename=${test_wave}"
+phrase="serwis testowy"
+check_output "${cmd}" "${phrase}"
+
+echo
+echo "Testing: --interim-results"
+cmd="${SCRIPTPATH}/../docker/run_dictation_client_cpp.sh --service-address=${test_service_address} --filename=${test_wave} --interim-results"
+phrase="serwis"
+check_output "${cmd}" "${phrase}"
+
+echo
+echo "Testing: --grpc-timeout"
+cmd="${SCRIPTPATH}/../docker/run_dictation_client_cpp.sh --service-address=${test_service_address} --filename=${test_wave} --grpc-timeout=50"
+phrase="DEADLINE_EXCEEDED"
+check_output "${cmd}" "${phrase}"
+
+echo
+echo "Testing: --time-offsets"
+cmd="${SCRIPTPATH}/../docker/run_dictation_client_cpp.sh --service-address=${test_service_address} --filename=${test_wave} --time-offsets"
+phrase="nanos: 60000000"
+check_output "${cmd}" "${phrase}"
+
+echo
+echo "Testing: --context-phrase"
+cmd="${SCRIPTPATH}/../docker/run_dictation_client_cpp.sh --service-address=${test_service_address} --filename=${test_context_wave} --context-phrase=context"
+phrase="przedsi\\\\304\\\\231wzi\\\\304\\\\231cie"
+check_output "${cmd}" "${phrase}"
+
+
+test_result="success"
