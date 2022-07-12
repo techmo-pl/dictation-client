6d19b86ee699e0650dbd546f6f3731d3d0305f1e
irx <5205126+irx@users.noreply.github.com>
2022-07-12 14:58:24 +0200

    Improve proto APIs building scripts (#73)

--- cpp/make_proto.sh
+++ cpp/make_proto.sh
@@ -4,29 +4,28 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "${SCRIPT}")
+SCRIPT="$(realpath "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
 
-grpc_root="/opt/grpc_v1.38.1"
+grpc_root="${GRPC_ROOT:-/opt/grpc_v1.38.1}"
 
-PROTOC="$grpc_root/build/third_party/protobuf/protoc"
-PROTOBUF="$grpc_root/third_party/protobuf/src"
-PLUGIN="--plugin=protoc-gen-grpc=$grpc_root/build/grpc_cpp_plugin"
+PROTOC="${PROTOC:-${grpc_root}/build/third_party/protobuf/protoc}"
+PROTOBUF="${grpc_root}/third_party/protobuf/src"
+PLUGIN="--plugin=protoc-gen-grpc=${GRPC_PLUGIN:-${grpc_root}/build/grpc_cpp_plugin}"
 GAPIS="${SCRIPTPATH}/../googleapis_files"
 
-if [ ! -x ${PROTOC} ]; then
-    echo "${PROTOC}; no such file."
-    exit 1
-fi
+[ ! -x "$(command -v "${PROTOC}" 2> /dev/null)" ] \
+	&& echo "${PROTOC}: executable not found" \
+	&& exit 1
 
 echo "Generating dictation C++ protobuf/grpc sources."
 path_i="${SCRIPTPATH}/../proto"
 path_o="${SCRIPTPATH}/libdictation-client"
-${PROTOC}   -I${path_i} \
-            -I${GAPIS} \
-            -I${PROTOBUF} \
-            ${PLUGIN} \
-            --cpp_out=${path_o} \
-            --grpc_out=${path_o} \
-            ${path_i}/dictation_asr.proto
-            
+"${PROTOC}" \
+	-I"${path_i}" \
+	-I"${GAPIS}" \
+	-I"${PROTOBUF}" \
+	"${PLUGIN}" \
+	--cpp_out="${path_o}" \
+	--grpc_out="${path_o}" \
+	"${path_i}/dictation_asr.proto"
